e6a74e42d58bc70dd6e2a92577323bf486a08dc4
unknown <alexandar.mihaylov@uoit.net>
2015-02-21 17:03:33 -0500

    notification template

--- www/js/notification.js
+++ www/js/notification.js
@@ -1,3 +1,12 @@
-function makeNotificaiton(message){
-    
+function notification(){
+
+    navigator.notification.alert(
+	'You are the winner!',  // message
+	alertDismissed,         // callback
+	'Game Over',            // title
+	'Done'                  // buttonName
+    );
+}
+function alertDismissed() {
+    // do something
 }
