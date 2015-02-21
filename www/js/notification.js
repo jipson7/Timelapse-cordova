73b4ff0eeb968027300a577bd0cbbe9772e3fe8c
jipson7 <caleb.phillips@uoit.net>
2015-02-21 17:37:07 -0500

    Added a test for push notiffications and fixed and error in index.html

--- www/js/notification.js
+++ www/js/notification.js
@@ -1,4 +1,12 @@
-function notification(){
+function notificationTest() {
+
+	$(".mainBody").append("<button onclick='pushNotification()'>Push It Real Good</button>");
+
+} 
+
+function pushNotification(){
+
+	$(".mainBody").append("<p>Attempting Push</p>");
 
     navigator.notification.alert(
 	'You are the winner!',  // message
