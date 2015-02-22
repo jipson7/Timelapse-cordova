e25a2a16860ac79898fa10a076e166dd280f66bd
ObjectiveTruth-Laptop <josemiguel.mendez@uoit.net>
2015-02-22 02:53:09 -0500

    added a comment

--- www/js/slideshowLogic.js
+++ www/js/slideshowLogic.js
@@ -1,9 +1,9 @@
 function slideshowLogic() {
     //These are temporary to test the page
-    var imageArr = ["http://i.imgur.com/dAvWkN8.jpg",
+   var imageArr = ["http://i.imgur.com/dAvWkN8.jpg",
         "http://i.imgur.com/Te0WuIB.jpg","http://i.imgur.com/Sfyljur.jpg"];
     var listHtml = "<ul class='bxslider'>";
-
+    //A for loop
     for (i = 0; i < imageArr.length; i++){
         listHtml = listHtml + "<li><img src='" + imageArr[i] + "'/></li>";
     }
